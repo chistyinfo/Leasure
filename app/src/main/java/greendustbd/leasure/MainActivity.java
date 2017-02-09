cfb80aba696b9654ec8328c70bf24e2c94b44b51
chisty <chistyinfo@gmail.com>
2017-02-09 08:23:10 +0600

    Layout desinging

--- app/src/main/java/greendustbd/leasure/MainActivity.java
+++ app/src/main/java/greendustbd/leasure/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,7 +14,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
         mv= (ImageView) findViewById(R.id.mv);
         bk= (ImageView) findViewById(R.id.bk);
         dr= (ImageView) findViewById(R.id.dr);
--- app/src/main/java/greendustbd/leasure/MovieActivity.java
+++ app/src/main/java/greendustbd/leasure/MovieActivity.java
@@ -9,6 +9,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
@@ -46,6 +48,9 @@ public class MovieActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_movie);
 
         listView = (ListView) findViewById(greendustbd.leasure.R.id.list);
